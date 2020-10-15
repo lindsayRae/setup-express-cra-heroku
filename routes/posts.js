11917c8a529f87c95f69cc89d3e5f7e69049eea5
lindsayRae <lbarnett712@gmail.com>
2020-10-15 08:03:15 -0400

    feat node serving react

--- routes/posts.js
+++ routes/posts.js
@@ -7,3 +7,4 @@ router.get('/', function (req, res) {
         {id:1, title: "Hello, from /routes/posts.js!"}
     ])
 })
+
--- server.js
+++ server.js
@@ -2,12 +2,37 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 1234
 
-app.get('/api', (req, res) => {
+console.log('in server PORT: ', port)
+console.log('in server process.env.NODE_ENV: ', process.env.NODE_ENV)
+
+app.get('/', (req, res) => {
   console.log('In get request ...')
   res.json([
       {id:1, title: "Hello, this is data from server.js!"}
   ])
 })
+
+app.get('/api', (req, res) => {
+  console.log('In get request ...')
+  res.json([
+      {id:1, title: "Hello, from /API "}
+  ])
+})
+
+if(process.env.NODE_ENV === 'production'){
+  // Serve the static files from the React app
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handles any requests that don't match the ones above
+  app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+}
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
